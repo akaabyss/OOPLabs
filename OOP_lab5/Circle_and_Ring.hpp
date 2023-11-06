b0ab7441b3ec7d48e01ffd766499e9f1cc23cbf9
Albarrasin <vaf_art@mail.ru>
2023-11-07 00:30:53 +0300

    may be done lab5

--- OOP_lab5/Circle_and_Ring.hpp
+++ OOP_lab5/Circle_and_Ring.hpp
@@ -10,23 +10,43 @@ protected:
     float r;
 
 public:
-    Circle(const float &_r = 1);
-    Circle(const Circle &object);
-    // virtual
-    float Square() const
+    Circle(const float &_r = 1)
+    {
+        r = _r;
+    }
+    Circle(const Circle &object)
+    {
+        r = object.r;
+    }
+
+    virtual float Square() const
     {
         return PI * r * r;
     }
-
-    void increase(const float &_delta);
-    // virtual
-    Circle &operator+(const float &delta)
+    virtual void increase(const float &_delta)
+    {
+        r *= _delta;
+    }
+    virtual Circle &operator+(const float &delta)
     {
         r += delta;
         return *this;
     }
-    friend ostream &operator<<(ostream &stream, const Circle &object);
-    friend istream &operator>>(istream &stream, Circle &object);
+
+    friend ostream &operator<<(ostream &stream, const Circle &object)
+    {
+        stream << "R = " + to_string(object.r) + ", Square = " + to_string(object.Square()) + "\n\n";
+        return stream;
+    }
+    friend istream &operator>>(istream &stream, Circle &object)
+    {
+        stream >> object.r;
+        return stream;
+    }
+    Circle operator=(const Circle &object)
+    {
+        return Circle(object.r);
+    }
 };
 
 class Ring : public Circle
@@ -35,17 +55,38 @@ class Ring : public Circle
 
 public:
     // Внешний, потом внутренний
-    Ring(const float &_r, const float &_rin);
+    Ring(const float &_r, const float &_rin) : Circle(_r), rin(_rin) {}
 
-    float Square() const;
+    float Square() const
+    {
+        return Circle::Square() - PI * rin * rin;
+    }
+    void increase(const float &_delta = 1)
+    {
+        Circle::increase(_delta);
+        rin *= _delta;
+    }
 
-    void increase(const float &_delta = 1);
     Ring &operator+(const float &delta)
     {
-        r += delta;
+        // r += delta;
+        *this = *this + 5; // Circle::operator+
         rin += delta;
         return *this;
     }
-    friend ostream &operator<<(ostream &stream, const Ring &object);
-    friend istream &operator>>(istream &stream, Ring &object);
+    friend ostream &operator<<(ostream &stream, const Ring &object)
+    {
+        stream << "R = " + to_string(object.r) + ", r = " + to_string(object.rin) + ", Square = " + to_string(object.Square()) + "\n\n";
+        return stream;
+    }
+    friend istream &operator>>(istream &stream, Ring &object)
+    {
+        // Внешний, потом внутренний радиус
+        stream >> object.r >> object.rin;
+        return stream;
+    }
+    Ring operator=(const Ring &object)
+    {
+        return Ring(object.r, object.rin);
+    }
 };
